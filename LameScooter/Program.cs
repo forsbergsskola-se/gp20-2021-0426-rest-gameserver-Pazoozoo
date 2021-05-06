15bd9e22ceb52a1bb46baeafa9e21e2376451491
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-06 10:44:45 +0200

    added rental interface

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace LameScooter
 {
@@ -6,7 +7,17 @@ namespace LameScooter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
+
+            ILameScooterRental rental = null; // Replace with new XXX() later.
+
+            // var count = await rental.GetScooterCountInStation(args[0]); 
+            
+            Console.WriteLine("Number of Scooters Available at this Station: "); // Add the count that is returned above to the output.
         }
+    }
+
+    public interface ILameScooterRental {
+        Task<int> GetScooterCountInStation(string stationName);
     }
 }
