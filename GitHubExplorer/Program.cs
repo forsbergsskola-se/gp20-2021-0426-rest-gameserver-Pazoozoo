3459cc5c58bd21cceef57b81afef1678cf14b6b6
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-10 14:57:03 +0200

    added basic user input

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.Http;
 
-namespace GitHubExplorer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace GitHubExplorer {
+    class Program {
+        HttpClient Client = new HttpClient();
+        static void Main(string[] args) {
+
+            Console.WriteLine("Choose REST API:");
+            Console.WriteLine("0: GitHub");
+            var userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput) {
+                case 0:
+                    Console.WriteLine("GitHub API");
+                    break;
+                default:
+                    Console.WriteLine("No API found");
+                    break;
+            }
         }
     }
 }
