9a64a0bb93bbdb5226f2220f93a9bcea6f802c35
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-10 16:14:46 +0200

    added try catch exception handling for invalid user name input

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -9,8 +9,17 @@ using System.Threading.Tasks;
 namespace GitHubExplorer {
     class Program {
         static readonly HttpClient Client = new HttpClient();
+        static string _userName;
         static async Task Main(string[] args) {
-
+            
+            while (true) {
+                Console.WriteLine("Enter GitHub UserID:");
+                _userName = Console.ReadLine();
+                if (_userName != null) break;
+                Console.WriteLine("No input received.\n");
+            }
+            
+            
             Console.WriteLine("Choose REST API:");
             Console.WriteLine("0: GitHub");
             // var userInput = int.Parse(Console.ReadLine());
@@ -18,7 +27,7 @@ namespace GitHubExplorer {
             switch (0) {
                 case 0:
                     Console.WriteLine("GitHub API");
-                    await RunGitHubApi();
+                    await GitHubRepositories();
                     break;
                 default:
                     Console.WriteLine("No API found");
@@ -26,8 +35,10 @@ namespace GitHubExplorer {
             }
         }
 
-        static async Task RunGitHubApi() {
+        static async Task GitHubRepositories() {
             var repos = await FetchRepositories();
+            if (repos.Count == 0) 
+                return;
             Console.WriteLine("Repos:\n");
             foreach (var repo in repos) {
                 Console.WriteLine($"Name: {repo.Name}");
@@ -43,10 +54,15 @@ namespace GitHubExplorer {
             Client.DefaultRequestHeaders.Clear();
             Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             Client.DefaultRequestHeaders.Add("User-Agent", "GitHub Repository Finder");
-
-            var streamTask = Client.GetStreamAsync("https://api.github.com/users/Pazoozoo/repos");
-            var repos = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            var repos = new List<Repository>();
             
+            try {
+                var streamTask = Client.GetStreamAsync($"https://api.github.com/users/{_userName}/repos");
+                repos = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            }
+            catch (HttpRequestException e) {
+                Console.WriteLine("User not found... Error message: " + e.Message);
+            }
             return repos;
         }
     }
