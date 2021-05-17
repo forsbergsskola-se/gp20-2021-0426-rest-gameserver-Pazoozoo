b2edd6f2643e8401451b74b1947cc1995f2567ad
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-17 22:15:27 +0200

    ajusted program to use the new method instead of constructors

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -9,22 +9,28 @@ namespace LameScooter
             string database = args[1];
             database = database.ToLower();
             ILameScooterRental rental;
+            string uri;
 
             switch (database) {
                 case "offline":
-                    rental = new OfflineLameScooterRental("scooters.json");
+                    rental = new OfflineLameScooterRental();
+                    uri = "scooters.json";
                     break;
                 case "deprecated":
-                    rental = new DeprecatedLameScooterRental("scooters.txt");
+                    rental = new DeprecatedLameScooterRental();
+                    uri = "scooters.txt";
                     break;
                 case "realtime":
-                    rental = new RealTimeLameScooterRental(
-                        "https://raw.githubusercontent.com/marczaku/GP20-2021-0426-Rest-Gameserver/main/assignments/scooters.json");
+                    rental = new RealTimeLameScooterRental();
+                    uri = "https://raw.githubusercontent.com/marczaku/GP20-2021-0426-Rest-Gameserver/main/assignments/scooters.json";
                     break;
                 default:
-                    rental = new OfflineLameScooterRental("scooters.json");
+                    rental = new OfflineLameScooterRental();
+                    uri = "scooters.json";
                     break;
             }
+
+            rental.GetScooterDatabase(uri).GetAwaiter().GetResult();
 
             try {
                 Console.WriteLine($"Number of Scooters Available at {station} Station: " +
