6d87cf89cb5cb4a9580d0892a8330fc0861a1e37
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-17 20:56:47 +0200

    implemented reading txt into a dictionary and the GetScooterCountInStation method

--- LameScooter/DeprecatedLameScooterRental.cs
+++ LameScooter/DeprecatedLameScooterRental.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LameScooter {
     public class DeprecatedLameScooterRental : ILameScooterRental {
-        Dictionary<string, LameScooterStationList> _scooterList;
+        Dictionary<string, int> _scooterDictionary;
         public DeprecatedLameScooterRental(string uri) {
-            Console.WriteLine(uri);
             if (!File.Exists(uri)) {
                 Console.Write($"File not found in path: {uri}");
                 return;
@@ -16,16 +16,30 @@ namespace LameScooter {
         }
 
         void ReadFile(string uri) {
-            using (StreamReader sr = new StreamReader(uri)) {
-                string line;
-                while ((line = sr.ReadLine()) != null) {
-                    Console.WriteLine(line);
-                }
+            _scooterDictionary = new Dictionary<string, int>();
+            
+            using var streamReader = new StreamReader(uri);
+            
+            while (streamReader.Peek() >= 0) {
+                var line = streamReader.ReadLine();
+                if (line == null) continue;
+                var index = line.IndexOf(':');
+                var station = line.Substring(0, index - 1);
+                var bikesAvailable = int.Parse(line.Substring(index + 2));
+                _scooterDictionary[station] = bikesAvailable;
             }
         }
         
         public Task<int> GetScooterCountInStation(string stationName) {
-            throw new NotImplementedException();
+            if (stationName.Any(char.IsDigit)) 
+                throw new ArgumentException($"{stationName} contains a digit.");
+
+            try {
+                return Task.FromResult(_scooterDictionary[stationName]);
+            }
+            catch (KeyNotFoundException e) {
+                throw new NotFoundException(stationName);
+            }
         }
     }
 }
