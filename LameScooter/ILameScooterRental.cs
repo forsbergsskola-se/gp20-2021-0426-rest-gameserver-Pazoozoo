018fb4441e84fb2cc282625be45126a32b0947e5
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-17 22:14:44 +0200

    added Task to interface to move logic from constructors to a Task

--- LameScooter/ILameScooterRental.cs
+++ LameScooter/ILameScooterRental.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 
 namespace LameScooter {
     public interface ILameScooterRental {
+        Task GetScooterDatabase(string uri);
         Task<int> GetScooterCountInStation(string stationName);
     }
 }
