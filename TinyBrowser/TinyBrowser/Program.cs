3acd8f60da2056fa5a3e4367f5a443f86d2d506b
Ruben Rådelius <ruben.radelius@gmail.com>
2021-05-19 16:20:33 +0200

    added continuous browsing, improved user input loop, added uri history

--- TinyBrowser/TinyBrowser/Program.cs
+++ TinyBrowser/TinyBrowser/Program.cs
@@ -11,70 +11,90 @@ namespace TinyBrowser {
         static void Main(string[] arguments) {
             var host = "acme.com";
             var uri = "/";
-            // Here, acme.com is only used for DNS-Resolving and gives us an IP
-            var tcpClient = new TcpClient(host, 80);
-            var stream = tcpClient.GetStream();
-            var streamWriter = new StreamWriter(stream, Encoding.ASCII);
-            // Here, acme.com is passed to the Webserver at the IP, so it knows, which website to give us
-            // In case, that one computer hosts multiple websites (think of Web-Hosts like wix.com)
-        
-            /*
-         * GET / HTTP.1.1
-         * Host: acme.com
-         * Content-Length: 7
-         *
-         * abcdefg
-         */
-        
-            // This is a valid HTTP/1.1-Request to send:
-            var request = $"GET {uri} HTTP/1.1\r\nHost: {host}\r\n\r\n";
-            streamWriter.Write(request); // add data to the buffer
-            streamWriter.Flush(); // actually send the buffered data
+            var uriHistory = new Stack<string>();
 
-            var streamReader = new StreamReader(stream);
-            var response = streamReader.ReadToEnd();
-
-            var uriBuilder = new UriBuilder(null, host);
-            uriBuilder.Path = uri;
-            Console.WriteLine($"Opened {uriBuilder}");
-
-            var titleText = FindTextBetweenTags(response, "title");
-            Console.WriteLine("Title: " + titleText);
-            // Console.WriteLine("response: " + response);
-
-            var hrefLinks = GetTextBetweenStringsFromString(response, "<a href=\"", "\">");
-            var hrefLinkList = hrefLinks.ToList();
-            var hrefDescriptionList = new List<string>();
-
-            for (var i = 0; i < hrefLinkList.Count; i++) {
-                var description = GetTextBetweenStringsFromString(response, hrefLinkList[i] + "\">", "</a>");
-                hrefDescriptionList.AddRange(description);
-
-                Console.WriteLine($"{i}: {hrefDescriptionList[i]} ({hrefLinkList[i]})");
-            }
-            
-            var number = 0;
             while (true) {
-                Console.WriteLine($"Enter a number between 0-{hrefLinkList.Count - 1}:");
-                var input = TryParse(Console.ReadLine(), out number);
-                if (!input || number < 0 || number > hrefLinkList.Count - 1) 
-                    continue;
-                break;
+                var tcpClient = new TcpClient(host, 80);
+                var stream = tcpClient.GetStream();
+                var streamWriter = new StreamWriter(stream, Encoding.ASCII);
+        
+                /*
+                 * GET / HTTP.1.1
+                 * Host: acme.com
+                 * Content-Length: 7
+                 *
+                 * abcdefg
+                 */
+            
+                var request = $"GET {uri} HTTP/1.1\r\nHost: {host}\r\n\r\n";
+                streamWriter.Write(request); // add data to the buffer
+                streamWriter.Flush(); // actually send the buffered data
+
+                var streamReader = new StreamReader(stream);
+                var response = streamReader.ReadToEnd();
+
+                var uriBuilder = new UriBuilder(null, host);
+                uriBuilder.Path = uri;
+                Console.WriteLine($"Opened {uriBuilder}");
+
+                var titleText = FindTextBetweenTags(response, "title");
+                Console.WriteLine("Title: " + titleText);
+
+                var hrefLinks = GetTextBetweenStringsFromString(response, "<a href=\"", "\">");
+                var hrefLinkList = hrefLinks.ToList();
+                var hrefDescriptionList = new List<string>();
+
+                for (var i = 0; i < hrefLinkList.Count; i++) {
+                    var description = GetTextBetweenStringsFromString(response, hrefLinkList[i] + "\">", "</a>");
+                    hrefDescriptionList.AddRange(description);
+
+                    Console.WriteLine($"{i}: {hrefDescriptionList[i]} ({hrefLinkList[i]})");
+                }
+                
+                var userInput = 0;
+                while (true) {
+                    Console.WriteLine("Text commands: 'back', 'home', 'exit'");
+                    Console.WriteLine($"Enter a number between 0-{hrefLinkList.Count - 1} to navigate:");
+                    var rawInput = Console.ReadLine();
+
+                    if (!rawInput.Any(char.IsDigit)) {
+                        switch (rawInput) {
+                            case "back":
+                                uri = uriHistory.Count > 0 ? uriHistory.Pop() : "/";
+                                break;
+                            case "home":
+                                uri = "/";
+                                break;
+                            case "exit":
+                                userInput = -1;
+                                break;
+                            default:
+                                Console.WriteLine("Unknown command.\n");
+                                continue;
+                        }
+                        break;
+                    }
+                    
+                    var input = TryParse(rawInput, out userInput);
+                    if (!input || userInput < 0 || userInput > hrefLinkList.Count - 1) 
+                        continue;
+                    uriHistory.Push(uri);
+                    uri = "/" + hrefLinkList[userInput];
+                    break;
+                }
+                if (userInput == -1) 
+                    break;
             }
         }
         
         static string FindTextBetweenTags(string original, string tag) {
             var startTag = $"<{tag}>";
-            // Find the start of the <tag>
             var tagIndex = original.IndexOf(startTag);
             string result = string.Empty;
             if (tagIndex != -1) {
-                // Offset the index by the length of the <tag>, to omit it
                 tagIndex += startTag.Length;
-                // Find the start of the </tag>-End
                 var tagEndIndex = original.IndexOf($"</{tag}>");
                 if (tagEndIndex > tagIndex) {
-                    // Get the string in between both
                     result = original[tagIndex..tagEndIndex];
                 }
             }
@@ -94,13 +114,6 @@ namespace TinyBrowser {
                 yield return text[(startIndex + start.Length)..endIndex];
                 currentIndex = endIndex;
             }
-        }
-    }
-
-// Extension Method
-    public static class StringExtensions {
-        public static string FindTextBetweenTags(this string original, string start, string end) {
-            throw new NotImplementedException();
         }
     }
 }
